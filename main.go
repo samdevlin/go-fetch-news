9f748e7f6f3c8195632c73f71223f141c356de03
Sam Devlin <devsoftsam@gmail.com>
2025-08-17 20:01:52 +0200

    WIP: unmarshalling JSON

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"encoding/csv"
+	"encoding/xml"
 	"errors"
+	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
 
@@ -11,8 +15,51 @@ type source struct {
 	url  string
 }
 
+type Urlset struct {
+	Url []Url `xml:"url"`
+}
+
+type Url struct {
+	Loc  string `xml:"loc"`
+	News News   `xml:"news:news"`
+}
+
+type News struct {
+	Title    string `xml:"news:title"`
+	Keywords string `xml:"news:keywords"`
+}
+
 func main() {
-	loadCSV("sources.csv")
+	sources, err := loadCSV("sources.csv")
+	if err != nil {
+		fmt.Println("A fatal error occurred. Exiting..")
+	}
+
+	for _, s := range sources {
+		// Fetch the xml
+		resp, err := http.Get(s.url)
+		if err != nil {
+			fmt.Printf("An error occurred fetching source %s, skipping..", s.name)
+			continue
+		}
+
+		// Parse & process
+		defer resp.Body.Close()
+		data, err := io.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		var urlset Urlset
+		if err := xml.Unmarshal(data, &urlset); err != nil {
+			panic(err)
+		}
+
+		// TODO: emit an event
+		for _, item := range urlset.Url {
+			fmt.Printf("%s ==> %s\n", item.News.Title, item.Loc)
+		}
+	}
 }
 
 func loadCSV(filename string) ([]source, error) {
@@ -31,6 +78,7 @@ func loadCSV(filename string) ([]source, error) {
 	var res []source
 
 	for _, value := range records {
+		// TODO: handle out of range
 		res = append(res, source{value[0], value[1]})
 	}
 
